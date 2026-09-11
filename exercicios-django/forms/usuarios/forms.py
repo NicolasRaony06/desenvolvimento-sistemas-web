6685c7f47421c755d08bf63a53c683c226f1a1bc
NicolasRaony06 <nicolas.raony06@gmail.com>
2026-09-11 12:12:24 -0300

    Add: css no form

--- exercicios-django/forms/usuarios/forms.py
+++ exercicios-django/forms/usuarios/forms.py
@@ -1,8 +1,22 @@
 from django import forms
 
 class ContatoForm(forms.Form):
-    nome = forms.CharField(label="Seu nome", max_length=50)
-    email = forms.EmailField(label="Seu email")
+    nome = forms.CharField(label="Seu nome", max_length=50,
+            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Seu nome'})
+            )
+    email = forms.EmailField(label="Seu email",widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'nome@exemplo.com',
+            'autocomplete': 'email'
+            })
+            )
+    
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email.endswith('@nicolas.corp.com'):
+            raise forms.ValidationError("Apenas e-mails da corporação (@nicolas.corp.com) são permitidos")
+        return email
 
     
     
